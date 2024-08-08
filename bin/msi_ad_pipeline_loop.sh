a45c11a8e86a46406f0a2fbebf7f0d86c83620ce
Rene Labounek <rene.labounek@gmail.com>
2024-08-08 15:54:07 -0500

    Add single phase necoding distortion correction

--- bin/msi_ad_pipeline_loop.sh
+++ bin/msi_ad_pipeline_loop.sh
@@ -46,7 +46,8 @@ create_folders()
     fi
 }
 
-for SUB in sub-0060/ses-* sub-0073/ses-* sub-0105/ses-* sub-0115/ses-* sub-0117/ses-* sub-0120/ses-* sub-0126/ses-* sub-0127/ses-* sub-0134/ses-* sub-0137/ses-* sub-0141/ses-* sub-0142/ses-* sub-0144/ses-* sub-0149/ses-* sub-0185/ses-* sub-0050/ses-* sub-0065/ses-* sub-0058/ses-* sub-0074/ses-* sub-0085/ses-* sub-0099/ses-* sub-0104/ses-* sub-0106/ses-* sub-0112/ses-* sub-0136/ses-* sub-0161/ses-* ;do # sub-0060/ses-* sub-0073/ses-* sub-0105/ses-* sub-0115/ses-* sub-0117/ses-* sub-0120/ses-* sub-0126/ses-* sub-0127/ses-* sub-0134/ses-* sub-0137/ses-* sub-0141/ses-* sub-0142/ses-* sub-0144/ses-* sub-0149/ses-* sub-0185/ses-* sub-0050/ses-* sub-0065/ses-* sub-0058/ses-* sub-0074/ses-* sub-0085/ses-* sub-0099/ses-* sub-0104/ses-* sub-0106/ses-* sub-0112/ses-* sub-0136/ses-* sub-0161/ses-*
+for SUB in sub-0040/ses-* ;do
+#for SUB in sub-0060/ses-* sub-0073/ses-* sub-0105/ses-* sub-0115/ses-* sub-0117/ses-* sub-0120/ses-* sub-0126/ses-* sub-0127/ses-* sub-0134/ses-* sub-0137/ses-* sub-0141/ses-* sub-0142/ses-* sub-0144/ses-* sub-0149/ses-* sub-0185/ses-* sub-0050/ses-* sub-0065/ses-* sub-0058/ses-* sub-0074/ses-* sub-0085/ses-* sub-0099/ses-* sub-0104/ses-* sub-0106/ses-* sub-0112/ses-* sub-0136/ses-* sub-0161/ses-* ;do # sub-0060/ses-* sub-0073/ses-* sub-0105/ses-* sub-0115/ses-* sub-0117/ses-* sub-0120/ses-* sub-0126/ses-* sub-0127/ses-* sub-0134/ses-* sub-0137/ses-* sub-0141/ses-* sub-0142/ses-* sub-0144/ses-* sub-0149/ses-* sub-0185/ses-* sub-0050/ses-* sub-0065/ses-* sub-0058/ses-* sub-0074/ses-* sub-0085/ses-* sub-0099/ses-* sub-0104/ses-* sub-0106/ses-* sub-0112/ses-* sub-0136/ses-* sub-0161/ses-*
 	for NRD in 0;do # 0 1 2
 		for PRT in dmri;do #  dmri79 6shell
 			if [[ $SUB == "sub-779253/ses-13mo" && $PRT == "dmri79" ]]; then
@@ -72,9 +73,9 @@ for SUB in sub-0060/ses-* sub-0073/ses-* sub-0105/ses-* sub-0115/ses-* sub-0117/
                     for STAGE in `seq 1 9`;do
                         if [ $STAGE -eq 1 ];then
                             TIME="48:45:00"
-                            NTASKS=1
-                            MEM=2g
-                            TMP=2g
+                            NTASKS=2
+                            MEM=15g
+                            TMP=15g
                             QUEUE=msismall
                         elif [ $STAGE -eq 2 ];then
                             TIME="06:15:00"
