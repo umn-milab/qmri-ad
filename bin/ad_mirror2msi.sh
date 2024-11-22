a295eebfcb4ae908114964dd9c4648b8615907e5
Rene Labounek <rene.labounek@gmail.com>
2024-11-22 15:12:34 -0600

    update mirror

--- bin/ad_mirror2msi.sh
+++ bin/ad_mirror2msi.sh
@@ -2,6 +2,9 @@
 DATAFOLDER=/home/porto-raid4/nestrasil-data/ADAI
 MIRRORFOLDER=rlaboune@agate.msi.umn.edu:/home/shapiroe/shared/ADAI
 
+DATAFOLDER2=/home/range1-raid1/labounek/data-on-porto/ADNI/ADNI_ADAI_match
+MIRRORFOLDER2=rlaboune@agate.msi.umn.edu:/home/shapiroe/shared/ADNI/ADNI_ADAI_match
+
 rsyncommand="rsync -e ssh -avzur --no-o --no-g --relative"
 
 #if [ ! -d $MIRRORFOLDER ];then
@@ -11,3 +14,6 @@ rsyncommand="rsync -e ssh -avzur --no-o --no-g --relative"
 
 cd $DATAFOLDER
 `echo $rsyncommand bids $MIRRORFOLDER` # results/dmri
+
+cd $DATAFOLDER2
+`echo $rsyncommand bids $MIRRORFOLDER2` # results/dmri
